c3e3a74de30ad5f68045930340d83fbb2fbf9798
Tyler McKellar <tylermckellar@gmail.com>
2019-03-19 00:24:23 -0400

    Core functionality works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Card from "./components/Card";
 import pokemon from "./pokemon.json";
 import './App.css';
