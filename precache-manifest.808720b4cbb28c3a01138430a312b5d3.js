dee2cf8493075ac3d3dd813b7ed76ca3dec7b131
MistaWizard <tylermckellar@gmail.com>
2019-03-21 21:40:04 -0400

    Updates

---
+++ precache-manifest.808720b4cbb28c3a01138430a312b5d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c01537adc9220d32f8d3",
+    "url": "/GottaClickEmAll/static/js/runtime~main.7f3a0b33.js"
+  },
+  {
+    "revision": "951852271ade6146aea9",
+    "url": "/GottaClickEmAll/static/js/main.b1e598ff.chunk.js"
+  },
+  {
+    "revision": "a49af3237dbddbb747fb",
+    "url": "/GottaClickEmAll/static/js/2.650383ae.chunk.js"
+  },
+  {
+    "revision": "951852271ade6146aea9",
+    "url": "/GottaClickEmAll/static/css/main.bed4dae8.chunk.css"
+  },
+  {
+    "revision": "6ac0fc0b29c855054a22dfd8c5928083",
+    "url": "/GottaClickEmAll/index.html"
+  }
+];
